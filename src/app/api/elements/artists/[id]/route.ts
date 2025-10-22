833112dc01842462e52b7841ea317370b6e0dcb9
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-22 13:49:00 -0400

    feature: route for individual actions in artists

---
+++ src/app/api/elements/artists/[id]/route.ts
@@ -0,0 +1,26 @@
+
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+interface Params {
+  params: { id: string };
+}
+export async function GET(_: Request, { params }: Params) {
+  const artist = await prisma.artistEdited.findUnique({
+    where: { id: Number(params.id) },
+  });
+}
+
+export async function PUT(req: Request, { params }: Params) {
+  const data = await req.json();
+  const updated = await prisma.artistEdited.update({
+    where: { id: Number(params.id)},
+    data,
+  })
+  return NextResponse.json(updated);
+}
+
+export async function DELETE(_: Request, { params }: Params) {
+  await prisma.artistEdited.delete({ where: { id: Number(params.id)}});
+  return NextResponse.json({ success: true });
+}
