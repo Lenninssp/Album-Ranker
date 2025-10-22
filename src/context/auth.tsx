fc3c37ea054d757103032eb0a2bf024c2d5d3eb9
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-22 14:19:19 -0400

    actually, let's add also the ability to save the name

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -4,11 +4,13 @@ import { persist } from "zustand/middleware";
 type State = {
   authorized: boolean;
   userId: number | null;
+  userName: string;
 };
 
 type Actions = {
   toggle: () => void;
   setId: (id: number) => void;
+  setName: (name: string) => void;
 };
 
 export const useSession = create<State & Actions>()(
@@ -16,8 +18,10 @@ export const useSession = create<State & Actions>()(
     (set) => ({
       authorized: false,
       userId: null,
+      userName: "",
       toggle: () => set((state) => ({ authorized: !state.authorized })),
-      setId: (id: number) => set(() => ({ userId: id}))
+      setId: (id: number) => set(() => ({ userId: id})),
+      setName: (name: string) => set(() => ({userName: name})),
     }),
     {
       name: "session-storage",
