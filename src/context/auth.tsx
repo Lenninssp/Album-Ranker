dec3081d02ddc32530b96716e3b4205746f2e8cb
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-22 14:18:08 -0400

    feature: adding in the auth state the ability to store the user id

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -3,17 +3,21 @@ import { persist } from "zustand/middleware";
 
 type State = {
   authorized: boolean;
+  userId: number | null;
 };
 
 type Actions = {
   toggle: () => void;
+  setId: (id: number) => void;
 };
 
 export const useSession = create<State & Actions>()(
   persist(
     (set) => ({
       authorized: false,
+      userId: null,
       toggle: () => set((state) => ({ authorized: !state.authorized })),
+      setId: (id: number) => set(() => ({ userId: id}))
     }),
     {
       name: "session-storage",
