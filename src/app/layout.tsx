0552b64c24b48c8cfa4761c0839f608a51048eaf
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-23 23:57:04 -0400

    feature: adding session to access user intrinstict data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { AppInitializer } from "@/components/general/app-initializer";
+import { SessionProviderWrapper } from "@/components/general/session-provider-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-full`}
       >
-        {children}
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
         <Toaster />
       </body>
     </html>
