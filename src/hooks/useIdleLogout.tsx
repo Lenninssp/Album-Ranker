16f53edd3861a55c7269979e45e56605cbaddc2e
Lennin sabogal <lenninssp1021@gmail.com>
2025-10-23 23:08:19 -0400

    feature: automatic logout for session

---
+++ src/hooks/useIdleLogout.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+import { signOut } from 'next-auth/react'
+
+export function useIdelLogout(ms = 15 * 60 * 1000) {
+  const timer = useRef<number | null>(null);
+
+  useEffect(() => {
+    const reset = () => {
+      if (timer.current) window.clearTimeout(timer.current);
+      timer.current = window.setTimeout(() => {
+        signOut({ callbackUrl: "/auth" });
+      }, ms)
+    };
+
+    reset();
+
+    const events = ["click", "keydown", "mousemove", "scroll", "touchstart"];
+    events.forEach(ev => window.addEventListener(ev, reset, { passive: true}));
+
+    return () => {
+      if (timer.current) window.clearTimeout(timer.current);
+      events.forEach(ev => window.removeEventListener(ev, reset))
+    }
+  }, [ms]);
+}
