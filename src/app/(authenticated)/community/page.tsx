e3861c3591f35e750eec904f221fb8a98382caa9
Lennin sabogal <lenninssp1021@gmail.com>
2025-11-02 15:59:59 -0500

    feature: emptu pagefor the community rankings

---
+++ src/app/(authenticated)/community/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { DefaultFrame } from "@/components/default-frame"
+
+const CommunityPage = () => {
+
+  return (
+    <DefaultFrame className="flex items-center justify-center">
+      <div className="font-bold text-4xl">Community rankings</div>
+    </DefaultFrame>
+  )
+}
+
+export default CommunityPage;
