782f1aeb4e5ac880abc8fb3ca6b22999c1522ee5
Lennin sabogal <lenninssp1021@gmail.com>
2025-11-05 22:23:30 -0500

    re route of the interactions route

--- src/app/api/community/posts/interactions/route.ts
+++ src/app/api/community/posts/interactions/route.ts
@@ -67,47 +67,7 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export async function GET (
-  { params }: { params: Promise<{id: string}>}
-){
-  try {
-    const { id } = await params;
 
-    if ( !id || isNaN(Number(id))) {
-      return NextResponse.json(
-        { success: false, message: "Invalid or missing user ID in URL"},
-        { status: 400 }
-      )
-    }
-
-    const user = await prisma.user.findUnique( {
-      where: { id: Number(id)},
-    })
-
-
-    if (!user) {
-      return NextResponse.json(
-        { success: false, error: "User not found" },
-        { status: 404 }
-      );
-    }
-
-    const interactions = await prisma.interaction.findMany({
-      where: { userId: Number(id)}
-    })
-
-    return NextResponse.json(
-      interactions,
-      { status: 200 }
-    )
-  }
-  catch(err) {
-    return NextResponse.json(
-      {success: false, message: "Could not get interactions"},
-      { status: 500 }
-    )
-  }
-}
 
 /*
 
