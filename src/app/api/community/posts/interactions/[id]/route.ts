7edebaf055393514942bd4640b356c82cd049217
Lennin sabogal <lenninssp1021@gmail.com>
2025-11-06 08:37:40 -0500

    fix in get interactions

--- src/app/api/community/posts/interactions/[id]/route.ts
+++ src/app/api/community/posts/interactions/[id]/route.ts
@@ -1,23 +1,23 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET (
-  { params }: { params: Promise<{id: string}>}
-){
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const { id } = await params;
 
-    if ( !id || isNaN(Number(id))) {
+    if (!id || isNaN(Number(id))) {
       return NextResponse.json(
-        { success: false, message: "Invalid or missing user ID in URL"},
+        { success: false, message: "Invalid or missing user ID in URL" },
         { status: 400 }
-      )
+      );
     }
 
-    const user = await prisma.user.findUnique( {
-      where: { id: Number(id)},
-    })
-
+    const user = await prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
 
     if (!user) {
       return NextResponse.json(
@@ -27,18 +27,14 @@ export async function GET (
     }
 
     const interactions = await prisma.interaction.findMany({
-      where: { userId: Number(id)}
-    })
+      where: { userId: Number(id) },
+    });
 
+    return NextResponse.json(interactions, { status: 200 });
+  } catch (err) {
     return NextResponse.json(
-      interactions,
-      { status: 200 }
-    )
-  }
-  catch(err) {
-    return NextResponse.json(
-      {success: false, message: "Could not get interactions"},
+      { success: false, message: "Could not get interactions" },
       { status: 500 }
-    )
+    );
   }
 }
